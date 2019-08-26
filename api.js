f04ecf4cfe872de097314d092f991d92ad678c04
aman <aman@github.com>
2019-08-26 19:49:59 +0530

    intial commit

---
+++ api.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var app = express();
+var insta = require('instagram-node').instagram;
+
+//static files(css, js)
+app.use(express.static(__dirname+'/public'));
+
+//set view engine
+app.set('view engine','ejs');
+
+insta.use({
+    client_id : ' c8e249694daa489d9f2e9b81ee300f90',
+    client_secret : ' bfd6bfb3cfd54a518e67166c9bb16fef'
+});
+
+//redirect uri 
+var redirect = 'http://localhost:8080/handleAuth';
