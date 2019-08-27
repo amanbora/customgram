f1df50f431b4474ea87e976176b0d2ccfc6e097a
aman <aman@github.com>
2019-08-27 23:55:22 +0530

    bugs

--- api.js
+++ api.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var app = express();
-var insta = require('instagram-node').instagram();
 var pathRoutes = require('./api/routes/path');
 
 
@@ -10,14 +9,9 @@ app.use(express.static(__dirname+'/public'));
 //set view engine
 app.set('view engine','ejs');
 
-insta.use({
-    client_id : ' c8e249694daa489d9f2e9b81ee300f90',
-    client_secret : ' bfd6bfb3cfd54a518e67166c9bb16fef'
-});
-
-
 app.use('/',pathRoutes);
 
 app.listen(8080,()=>{
     console.log("listening at port 8080");
 });
+
--- api/routes/path.js
+++ api/routes/path.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
+var insta = require('instagram-node').instagram();
+var accessToken;
+
+
+insta.use({
+    client_id : ' c8e249694daa489d9f2e9b81ee300f90',
+    client_secret : ' bfd6bfb3cfd54a518e67166c9bb16fef'
+});
 
 
 //redirect uri 
-var redirect = 'http://localhost:8080/handleAuth';
+var redirectUri = 'http://localhost:8080/handleAuth';
 
 
+router.get('/authorize', function(req,res){
+    res.redirect(insta.get_authorization_url(redirectUri, {scope : ['public_content','likes']}));
+})
 
 router.get('/handleAuth',function(req,res){
     //retrieves the code that was passed along as a query to the '/handleAuth' route and uses this code to construct an access token
@@ -20,7 +31,7 @@ router.get('/handleAuth',function(req,res){
 
 router.get('/',function(req,res){
         insta.use({
-            access_token : accessToken
+            access_token: accessToken
         });
 
         insta.user_media_recent(access_token.split('.')[0], function(err, result, pagination, remaining, limit) {
